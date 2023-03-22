0e0c0e0dbce4c282a33efb94811accb2eda9627b
AlexSobral01 <alex.dacostasobral@gmail.com>
2023-03-22 08:02:38 -0300

    calculator with react

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "./Calculator.css";
+import Container from '@mui/material/Container';
+
+export default function Calculator() {
+
+  const [num, setNum] = useState(0);
+  const [oldnum, setOldNum] = useState(0);
+  const [operator, setOperator] = useState();
+
+  const inputNum = (e) => {
+    var input = e.target.value;
+    if (num === 0) {
+      setNum(input)
+    } else {
+      setNum(num + input)
+    }
+  }
+
+  const operatorHandler = (e) => {
+    var operator = e.target.value;
+    setOperator(operator);
+    setOldNum(num);
+    setNum(0);
+  }
+
+  const calculate = () => {
+    if (operator === "/") {
+      setNum(parseFloat(oldnum) / parseFloat(num));
+    } else if (operator === "X") {
+      setNum(parseFloat(oldnum) * parseFloat(num));
+    } else if (operator === "-") {
+      setNum(parseFloat(oldnum) - parseFloat(num));
+    } else if (operator === "+") {
+      setNum(parseFloat(oldnum) + parseFloat(num));
+    }
+  }
+
+  return (
+    <Container className='Container' maxWidth="xs">
+      <div>
+        <h1>{num}</h1>
+        <div>
+          <button onClick={inputNum} value={7}>7</button>
+          <button onClick={inputNum} value={8}>8</button>
+          <button onClick={inputNum} value={9}>9</button>
+          <button onClick={operatorHandler} value="/">/</button>
+          <button onClick={inputNum} value={4}>4</button>
+          <button onClick={inputNum} value={5}>5</button>
+          <button onClick={inputNum} value={6}>6</button>
+          <button onClick={operatorHandler} value="X">x</button>
+          <button onClick={inputNum} value={1}>1</button>
+          <button onClick={inputNum} value={2}>2</button>
+          <button onClick={inputNum} value={3}>3</button>
+          <button onClick={operatorHandler} value="-">-</button>
+          <button onClick={inputNum} value={0}>0</button>
+          <button onClick={inputNum} value={"."}>.</button>
+          <button  onClick={calculate} 
+          value="=">=</button>
+          <button onClick={operatorHandler} value="+">+</button>
+        </div>
+      </div>
+    </Container>
+  )
+}
